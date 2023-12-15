38111fdcd9e371d1b412c9e9c6a01d1bae90809a
Vincent Michel <vxgmichel@gmail.com>
2023-12-15 09:21:32 +0100

    Use struct for cleaner recursive DP

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -4,62 +4,73 @@ use std::{
     iter,
 };
 
-fn rec(
-    xs: &[u8],
-    ys: &[usize],
+struct Rec<'a, 'b> {
+    xs: &'a [u8],
+    ys: &'b [usize],
+    xs_len: usize,
+    ys_len: usize,
     safe_index: usize,
-    cache: &mut HashMap<(usize, usize, usize), u64>,
-    i: usize,
-    j: usize,
-    c: usize,
-) -> u64 {
-    // Stop condition
-    if i == xs.len() {
-        return (j == ys.len() || (j == ys.len() - 1 && ys[j] == c)) as u64;
-    }
-    if j == ys.len() {
-        return (i >= safe_index) as u64;
-    }
-    // Check cache
-    if let Some(&result) = cache.get(&(i, j, c)) {
-        return result;
-    }
-    // Recursion
-    let char = xs[i];
-    let count = ys[j];
-    let result = match (char, c) {
-        (b'.', 0) => rec(xs, ys, safe_index, cache, i + 1, j, 0),
-        (b'.', _) if c == count => rec(xs, ys, safe_index, cache, i + 1, j + 1, 0),
-        (b'.', _) => 0,
-        (b'#', _) if c == count => 0,
-        (b'#', _) => rec(xs, ys, safe_index, cache, i + 1, j, c + 1),
-        (b'?', 0) => {
-            rec(xs, ys, safe_index, cache, i + 1, j, 0)
-                + rec(xs, ys, safe_index, cache, i + 1, j, 1)
-        }
-        (b'?', _) if c == count => rec(xs, ys, safe_index, cache, i + 1, j + 1, 0),
-        (b'?', _) => rec(xs, ys, safe_index, cache, i + 1, j, c + 1),
-        _ => unreachable!(),
-    };
-    // Populate cache
-    cache.insert((i, j, c), result);
-    result
+    cache: HashMap<(usize, usize, usize), u64>,
 }
 
-fn count_arrangements(xs: &[u8], ys: &[usize]) -> u64 {
-    let mut cache: HashMap<_, u64> = HashMap::new();
-    let safe_index = xs
-        .iter()
-        .enumerate()
-        .filter(|(_, &x)| x == b'#')
-        .map(|(i, _)| i + 1)
-        .last()
-        .unwrap_or(0);
-    rec(xs, ys, safe_index, &mut cache, 0, 0, 0)
+impl<'a, 'b> Rec<'a, 'b> {
+    fn new(xs: &'a [u8], ys: &'b [usize]) -> Self {
+        let safe_index = xs
+            .iter()
+            .enumerate()
+            .filter(|(_, &x)| x == b'#')
+            .map(|(i, _)| i + 1)
+            .last()
+            .unwrap_or(0);
+        let cache = HashMap::new();
+        Self {
+            xs,
+            ys,
+            xs_len: xs.len(),
+            ys_len: ys.len(),
+            safe_index,
+            cache,
+        }
+    }
+
+    fn rec(&mut self, i: usize, j: usize, c: usize) -> u64 {
+        // Stop condition
+        if i == self.xs_len {
+            return (j == self.ys_len || (j == self.ys_len - 1 && self.ys[j] == c)) as u64;
+        }
+        if j == self.ys_len {
+            return (i >= self.safe_index) as u64;
+        }
+        // Check cache
+        if let Some(&result) = self.cache.get(&(i, j, c)) {
+            return result;
+        }
+        // Recursion
+        let char = self.xs[i];
+        let count = self.ys[j];
+        let result = match (char, c) {
+            (b'.', 0) => self.rec(i + 1, j, 0),
+            (b'.', _) if c == count => self.rec(i + 1, j + 1, 0),
+            (b'.', _) => 0,
+            (b'#', _) if c == count => 0,
+            (b'#', _) => self.rec(i + 1, j, c + 1),
+            (b'?', 0) => self.rec(i + 1, j, 0) + self.rec(i + 1, j, 1),
+            (b'?', _) if c == count => self.rec(i + 1, j + 1, 0),
+            (b'?', _) => self.rec(i + 1, j, c + 1),
+            _ => unreachable!(),
+        };
+        // Populate cache
+        self.cache.insert((i, j, c), result);
+        result
+    }
+
+    fn count_arrangements(xs: &'a [u8], ys: &'b [usize]) -> u64 {
+        Self::new(xs, ys).rec(0, 0, 0)
+    }
 }
 
 fn solve1(xs: &[(Vec<u8>, Vec<usize>)]) -> u64 {
-    xs.iter().map(|(x, y)| count_arrangements(x, y)).sum()
+    xs.iter().map(|(x, y)| Rec::count_arrangements(x, y)).sum()
 }
 
 fn solve2(xs: &[(Vec<u8>, Vec<usize>)]) -> u64 {
@@ -72,7 +83,7 @@ fn solve2(xs: &[(Vec<u8>, Vec<usize>)]) -> u64 {
             }
             a.extend(x);
             let b: Vec<usize> = iter::repeat(y).take(5).flatten().cloned().collect();
-            count_arrangements(&a, &b)
+            Rec::count_arrangements(&a, &b)
         })
         .sum()
 }
